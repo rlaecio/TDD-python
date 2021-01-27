4cffa3138cce390acf17c27dbcbb90665b5f4ad1
Gitlab.network <roque.souza@portodigital.pt>
2021-01-27 02:15:03 +0000

    excecoes

--- src/leilao/test_leilao.py
+++ src/leilao/test_leilao.py
@@ -2,7 +2,7 @@ from unittest import TestCase
 from src.leilao.dominio import Usuario, Lance, Leilao
 
 
-class TestAvaliador(TestCase):
+class TestLeilao(TestCase):
 
     # test_quando_adicionados_em_ordem_crescente_deve_retornar_o_maior_e_o_menor_valor_de_um_lance
 
@@ -24,18 +24,14 @@ class TestAvaliador(TestCase):
         self.assertEqual(menor_valor_esperado, self.leilao.menor_lance)
         self.assertEqual(maior_valor_esperado, self.leilao.maior_lance)
 
-    def test_deve_retornar_o_maior_e_o_menor_valor_de_um_lance_quando_adicionados_em_ordem_decrescente(self):
-        yuri = Usuario('Yuri')
-        lance_do_yuri = Lance(yuri, 100.0)
+    def test_nao_deve_permitir_propor_um_lance_em_ordem_decrescente(self):
 
-        self.leilao.propoe(self.lance_do_gui)
-        self.leilao.propoe(lance_do_yuri)
+        with self.assertRaises(ValueError):
+            yuri = Usuario('Yuri')
+            lance_do_yuri = Lance(yuri, 100.0)
 
-        menor_valor_esperado = 100.0
-        maior_valor_esperado = 150.0
-
-        self.assertEqual(menor_valor_esperado, self.leilao.menor_lance)
-        self.assertEqual(maior_valor_esperado, self.leilao.maior_lance)
+            self.leilao.propoe(self.lance_do_gui)
+            self.leilao.propoe(lance_do_yuri)
 
     def test_deve_retornar_o_mesmo_valor_para_o_maior_e_menor_lance_quando_leilao_tiver_um_lance(self):
         self.leilao.propoe(self.lance_do_gui)
@@ -60,12 +56,11 @@ class TestAvaliador(TestCase):
         self.assertEqual(menor_valor_esperado, self.leilao.menor_lance)
         self.assertEqual(maior_valor_esperado, self.leilao.maior_lance)
 
-    # se o leilão não tiver lances, deve permitir propor um lance
     def test_deve_permitir_propor_um_lance_caso_o_leilao_nao_tenha_lances(self):
         self.leilao.propoe(self.lance_do_gui)
+
         quantidade_de_lances_recebido = len(self.leilao.lances)
         self.assertEqual(1, quantidade_de_lances_recebido)
-
 
     def test_deve_permitir_propor_um_lance_caso_o_ultimo_usuario_seja_diferente(self):
         yuri = Usuario('Yuri')
@@ -75,6 +70,7 @@ class TestAvaliador(TestCase):
         self.leilao.propoe(lance_do_yuri)
 
         quantidade_de_lances_recebido = len(self.leilao.lances)
+
         self.assertEqual(2, quantidade_de_lances_recebido)
 
     def test_nao_deve_permitir_propor_lance_caso_o_usuario_seja_o_mesmo(self):
@@ -83,8 +79,3 @@ class TestAvaliador(TestCase):
         with self.assertRaises(ValueError):
             self.leilao.propoe(self.lance_do_gui)
             self.leilao.propoe(lance_do_gui200)
-            #self.fail(msg='Não lançou uma exceção')
-        #except ValueError:
-            quantidade_de_lances_recebido = len(self.leilao.lances)
-            self.assertEqual(1, quantidade_de_lances_recebido)
-
